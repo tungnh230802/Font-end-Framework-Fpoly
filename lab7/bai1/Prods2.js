5763021a2151892884bf28a36276602df17b61bb
tungnh <tungnh230802@gmail.comm>
2021-07-27 19:23:00 +0700

    change

---
+++ lab7/bai1/Prods2.js
@@ -0,0 +1,10 @@
+﻿[
+  {
+    "name": "Aniseed Syrup",
+    "price": 190,
+    "discount": 0.19,
+    "date": "2000-03-16",
+  },
+  { "name": "Change", "price": 19, "discount": 0, "date": "1982-12-18" },
+  { "name": "Aniseed Syrup", "price": 10, "discount": 0, "date": "1973-06-14" }
+]
